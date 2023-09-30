5f6c8cf07737a0cc43dc52e731499765d220e9b8
Jakub Czarski <jakub.czarski@gmail.com>
2023-09-30 13:07:55 +0200

    Creating Mango.AuthApi, jwt tokens, login, register, assignrole endpoints

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,67 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Services.IServices;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        protected ResponseDto _response;
+
+        public AuthAPIController(IAuthService authService)
+        {
+            _authService = authService;
+            _response = new ResponseDto();
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegistrationRequestDto model)
+        {
+            var resultMessage = await _authService.Register(model);
+            if (!string.IsNullOrEmpty(resultMessage))
+            {
+                _response.IsSuccess = false;
+                _response.Message = resultMessage;
+                return BadRequest(_response);
+
+            }
+            return Ok(_response);
+        }
+
+
+        [HttpPost("AssignRole")]
+        public async Task<IActionResult> AssignRole([FromBody] RegistrationRequestDto model)
+        {
+            var assignRoleSuccesfull = await _authService.AssignRole(model.Email.ToUpper(), model.Role.ToUpper());
+            if (!assignRoleSuccesfull)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Error encountered";
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+        {
+            var loginResponse = await _authService.Login(model);
+            if (loginResponse.User == null)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Username or password is incorrect";
+                return BadRequest(_response);
+            }
+            _response.Result = loginResponse;
+            return Ok(_response);
+        }
+
+
+    }
+}
